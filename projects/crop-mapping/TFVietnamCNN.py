7e676cbbf4583e99eb311375cf93463429e4837c
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-08-15 09:13:41 -0400

    Adding resnet unet with dice+focal loss

--- projects/crop-mapping/TFVietnamCNN.py
+++ projects/crop-mapping/TFVietnamCNN.py
@@ -426,11 +426,11 @@ class TFVietnamCNN(ConfigYAML, ToolBelt):
 
             # loss
             # self.loss = ToolBelt.dice_loss
-            # self.loss = sm.losses.DiceLoss(class_weights=self.weights) + \
-            #    (1 * sm.losses.CategoricalFocalLoss())
+            self.loss = sm.losses.DiceLoss(class_weights=self.weights) + \
+                (1 * sm.losses.CategoricalFocalLoss())
             # self.loss = sm.losses.DiceLoss()  # class_weights=self.weights)
             # self.loss = sm.losses.categorical_focal_jaccard_loss
-            self.loss = 'categorical_crossentropy'
+            # self.loss = 'categorical_crossentropy'
             # self.loss = 'sparse_categorical_crossentropy'
             # self.loss = jaccard_distance_loss
 
