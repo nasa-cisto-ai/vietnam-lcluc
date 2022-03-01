3d13cf6c109cbba84d9a2db808eecc55fc029fc5
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2022-03-01 18:04:01 -0500

    Adding small model to test

--- vietnam_lcluc/model/utils.py
+++ vietnam_lcluc/model/utils.py
@@ -157,15 +157,21 @@ def spline_window(window_size: int, power: int = 2):
     https://www.wolframalpha.com/input/?i=y%3Dx**2,+y%3D-(x-2)**2+%2B2,+y%3D(x-4)**2,+from+y+%3D+0+to+2
     """
     intersection = int(window_size/4)
-    wind_outer = (abs(2*(scipy.signal.triang(window_size))) ** power)/2
+    tria = scipy.signal.triang(window_size)
+    wind_outer = (abs(2*(tria)) ** power)/2
     wind_outer[intersection:-intersection] = 0
 
-    wind_inner = 1 - (abs(2*(scipy.signal.triang(window_size) - 1)) ** power)/2
+    wind_inner = 1 - (abs(2*(tria - 1)) ** power)/2
     wind_inner[:intersection] = 0
     wind_inner[-intersection:] = 0
 
     wind = wind_inner + wind_outer
     wind = wind / np.average(wind)
+    print(wind.shape)
+    # additions from Jordan
+    wind = np.expand_dims(np.expand_dims(wind, 1), 2)
+    wind = wind * wind.transpose(1, 0, 2)
+    print(wind.shape)
     return wind
 
 
